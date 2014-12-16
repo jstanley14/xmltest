f1a210e339de69b95deb08c349ea7c302119bf79
jstanley14 <jstanley@middil.com>
2014-12-16 16:32:04 -0500

    SPIK-262 - well, it's ugly but it parses

--- src/main/scala/com/jake/xmltest/ActivityParser.scala
+++ src/main/scala/com/jake/xmltest/ActivityParser.scala
@@ -1,8 +1,106 @@
 package com.jake.xmltest
 
+import scala.xml.NodeSeq
+
 object ActivityParser {
   def main(args: Array[String]): Unit = {
     val activityXML = scala.xml.XML.loadFile("./activity.xml")
-    println(activityXML \\ "element")
+    //println(activityXML \\ "element")
+
+    val image = parseImage(activityXML \\ "image")
+ //   println(image)
+
+    println(parseData(activityXML))
   }
+
+  def parseData(xml: NodeSeq): Data = {
+    val activityType = (xml \ "@type").text match { case "HMT" => HMT }
+    val author = (xml \ "@author").text
+    val saved = (xml \ "@saved").text
+    val version = (xml \ "@version").text.toDouble
+    val activity = parseActivity(xml \ "activity")
+
+    Data(activityType, author, saved, version, activity)
+  }
+
+  def parseActivity(xml: NodeSeq): Activity = {
+    val weight = parseWeight(xml \ "weight")
+    val metadata = parseMetadata(xml \ "metadata")
+    val containers = (xml \\ "container") map parseContainer
+
+    Activity(weight, metadata, containers)
+  }
+
+  def parseWeight(xml: NodeSeq): Weight = {
+    val total = (xml \ "total").text.toInt
+    val extra = (xml \ "extra").text.toInt
+
+    Weight(total, extra)
+  }
+
+  def parseMetadata(xml: NodeSeq): Metadata = {
+    val _type = (xml \ "type").text match { case "HMT" => HMT }
+
+    Metadata(_type)
+  }
+
+  def parseContainer(xml: NodeSeq): Container = {
+    val dimensions = Dimensions((xml \ "@x").text.toInt, (xml \ "@y").text.toInt,
+      (xml \ "@width").text.toInt, (xml \ "@height").text.toInt)
+    val borderStyle = (xml \ "@borderStyle").text
+    val borderColor = (xml \ "@borderColor").text.toInt
+    val backgroundColor = (xml \ "@backgroundColor").text.toInt
+    val backgroundAlpha = (xml \ "@backgroundAlpha").text.toInt
+    val borderPadding = (xml \ "@borderPadding").text.toInt
+    val borderThickness = (xml \ "@borderThickness").text.toInt
+    val borderRadius = (xml \ "@borderRadius").text.toInt
+    val rotation = (xml \ "@rotation").text.toInt
+    val appearance = Appearance(borderStyle, borderColor, backgroundColor,
+                                backgroundAlpha, borderPadding, borderThickness,
+                                borderRadius, rotation)
+    val containerType = (xml \ "@type").text
+    val elements = (xml \\ "element") map parseElement
+
+    Container(dimensions, appearance, containerType, elements)
+  }
+
+  def parseElement(xml: NodeSeq): Element = {
+    val elementType = parseElementType(xml \ "type")
+    val elementNodes = (xml \ "_") filterNot { _.label == "type" }
+
+    elementNodes.head.label match {
+      case "image" => {
+        val image = parseImage(elementNodes)
+        ImageElement(elementType, image)
+      }
+      case _ =>
+        throw new IllegalArgumentException("only image element parsing implemented")
+    }
+  }
+
+  def parseElementType(xml: NodeSeq): ElementType = {
+    ElementType(xml.text, (xml \ "@toolTip").text)
+  }
+
+  def parseImage(imageXML: NodeSeq): Image = {
+    val autoplay = stringToBool((imageXML \ "@autoplay").text)
+    val maintainAspect = stringToBool((imageXML \ "@maintainAspect").text)
+    val mute = stringToBool((imageXML \ "@mute").text)
+
+    val thumbnail = (imageXML \ "thumbnail").text
+    val audio = (imageXML \ "audio").text
+    val source = (imageXML \ "source").text
+
+    val url = (imageXML \ "url").text
+
+    Image(autoplay, maintainAspect, mute,
+      Seq(Url(url), Thumbnail(thumbnail), Audio(source)))
+  }
+
+  def stringToBool(s: String): Boolean = s match {
+    case "true" => true
+    case _ => false
+  }
+
 }
+
--- src/main/scala/com/jake/xmltest/Tags.scala
+++ src/main/scala/com/jake/xmltest/Tags.scala
@@ -1,8 +1,6 @@
 package com.jake.xmltest
 
-import java.util.Date
-
-case class Data(activityType: ActivityType, author: String, saved: Date,
+case class Data(activityType: ActivityType, author: String, saved: String,
                 version: Double, activity: Activity)
 case class Activity(weight: Weight, metadata: Metadata, containers: Seq[Container])
 case class Weight(total: Int, extra: Int)
@@ -28,43 +26,3 @@ sealed trait Tag
 case class Url(path: String) extends Tag
 case class Thumbnail(path: String) extends Tag
 case class Audio(source: String) extends Tag
-
-
-
-
-
-
-
-
-//import java.util.Date
-//
-//sealed trait ElementType
-//
-//case class Source(source: String)
-//case class Audio(source: Source)
-//case class Thumbnail(path: String)
-//case class Url(path: String)
-//
-//case class Image(autoplay: Boolean, maintainAspect: Boolean, mute: Boolean,
-//                  url: Url, thumbnail: Thumbnail, audio: Audio) extends ElementType
-//
-//case class Type(_type: String, tooltip: String)
-//
-//case class Element(_type: Type, elm: ElementType)
-//case class Elements(elms: Seq[Element])
-//
-//case class Container(x: Int, y: Int, width: Int, height: Int, borderStyle: String,
-//                      borderColor: Int, backgroundColor: Int, backgroundAlpha: Int,
-//                      borderPadding: Int, borderThickness: Int, borderRadius: Int,
-//                      rotation: Int, _type: String)
-//case class Containers(containers: Seq[Container])
-//
-//case class Metadata(_type: Type)
-//
-//case class Total(pts: Int)
-//case class Extra(pts: Int)
-//case class Weight(total: Total, extra: Extra)
-//
-//case class Activity(weight: Weight, metadata: Metadata, containers: Containers)
-//
-//case class Data(_type: Type, author: String, saved: Date, version: Double, activity: Activity)
